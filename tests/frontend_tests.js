e82ecd39a90000958c6ff780573697259f6cf7d5
prashant <prashant.bansal@itinker.io>
2025-02-08 00:13:53 +0530

    Updated frontend_tests.js

--- tests/frontend_tests.js
+++ tests/frontend_tests.js
@@ -8,21 +8,25 @@ import Login from '../frontend/web/src/pages/login';
 describe('Frontend Component Tests', () => {
   test('renders Navbar component', () => {
     render(<Navbar />);
+    screen.debug();
     expect(screen.getByRole('navigation')).toBeInTheDocument();
   });
 
   test('renders Footer component', () => {
     render(<Footer />);
+    screen.debug();
     expect(screen.getByText(/Copyright/i)).toBeInTheDocument();
   });
 
   test('renders Dashboard page', () => {
     render(<Dashboard />);
+    screen.debug();
     expect(screen.getByText(/Welcome to Dashboard/i)).toBeInTheDocument();
   });
 
   test('renders Login page', () => {
     render(<Login />);
+    screen.debug();
     expect(screen.getByLabelText(/Email/i)).toBeInTheDocument();
     expect(screen.getByLabelText(/Password/i)).toBeInTheDocument();
   });
